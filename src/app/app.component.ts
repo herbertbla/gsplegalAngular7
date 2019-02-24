7a8aabd0863f57a609de38f4603c1e2088cdb47b
herbert.blaschek@brz.gv.at <herbert.blaschek@brz.gv.at>
2019-02-24 22:54:01 +0100

    Umschalten englisch deutsch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,12 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'gsplegalAngular7';
+  germanSelected = false;
+
+  changeLanguage = function (key) {
+    this.germanSelected = (key === 'de');
+    this.translate.use(key);
+  };
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('de');
