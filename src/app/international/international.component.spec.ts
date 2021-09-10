cf7ad34366de8c2a5f5e0be7da5a96569aae88f4
Herbert Blaschek <herbert.blaschek@brz.gv.at>
2021-09-10 17:10:50 +0200

    Update 2021/09 - Angular 10

--- src/app/international/international.component.spec.ts
+++ src/app/international/international.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InternationalComponent } from './international.component';
 
@@ -6,7 +6,7 @@ describe('InternationalComponent', () => {
   let component: InternationalComponent;
   let fixture: ComponentFixture<InternationalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ InternationalComponent ]
     })
--- src/app/taetigkeit/taetigkeit.component.spec.ts
+++ src/app/taetigkeit/taetigkeit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TaetigkeitComponent } from './taetigkeit.component';
 
@@ -6,7 +6,7 @@ describe('TaetigkeitComponent', () => {
   let component: TaetigkeitComponent;
   let fixture: ComponentFixture<TaetigkeitComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TaetigkeitComponent ]
     })
