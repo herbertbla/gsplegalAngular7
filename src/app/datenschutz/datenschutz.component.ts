821055595a71925dda080ea832dfa43be6ed9451
Herbert Blaschek <herbert.blaschek@brz.gv.at>
2022-08-09 23:24:58 +0200

    Datenschutz

--- src/app/datenschutz/datenschutz.component.ts
+++ src/app/datenschutz/datenschutz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GspTranslationServiceService} from "../services/gsp-translation-service.service";
 
 @Component({
   selector: 'app-datenschutz',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatenschutzComponent implements OnInit {
 
-  constructor() { }
+  constructor( public gpsTS: GspTranslationServiceService) { }
 
   ngOnInit() {
   }
