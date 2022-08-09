4765bd64a3318976ffce2d654e072fa4ce7e2b64
Herbert Blaschek <herbert.blaschek@brz.gv.at>
2022-08-09 22:35:27 +0200

    Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GspTranslationServiceService} from "../services/gsp-translation-service.service";
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( public gpsTS: GspTranslationServiceService) { }
 
   ngOnInit() {
   }
